5991ff626dbea87cef4c4eba6d3b728ca3643d9e
valeria <valeriaacosta055@gmail.com>
2013-05-26 21:27:45 -0600

    termino del capitulo 7 sobre como crear un usario

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "valeria acosta"
+    email    "valeriaacosta055@gmail.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
